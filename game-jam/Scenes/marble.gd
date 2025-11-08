851efa6ada00003b6da2b1b995618d868de99c17
Ian Allenfort <ian.allenfort@wsu.edu>
2025-11-07 21:21:06 -0800

    Implemented Marble movement
    oh yeah, now we're gaming

--- game-jam/Scenes/marble.gd
+++ game-jam/Scenes/marble.gd
@@ -1,11 +1,28 @@
 extends RigidBody3D
+@onready var camera_3d: Camera3D = $"../Camera3D"
 
+var move_force :float = 10.0
+var max_ang_speed :float = 50.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _ready():
+	angular_damp = 1.5
 
+func _process(_delta):
+	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
+func _physics_process(_delta: float) -> void:
+	if Input.is_action_pressed("Forward"):
+		apply_torque(camera_3d.global_basis * Vector3(-1, 0, 0) * move_force)
+	if Input.is_action_pressed("Backward"):
+		apply_torque(camera_3d.global_basis * Vector3(1, 0, 0) * move_force)
+	if Input.is_action_pressed("Right"):
+		apply_torque(camera_3d.global_basis * Vector3(0, 0, -1) * move_force)
+	if Input.is_action_pressed("Left"):
+		apply_torque(camera_3d.global_basis * Vector3(0, 0, 1) * move_force)
+
+	# clamp rotational velocity
+	if angular_velocity.length() > max_ang_speed:
+		angular_velocity = angular_velocity.normalized() * max_ang_speed
+
+func _exit_tree() -> void:
 	pass
