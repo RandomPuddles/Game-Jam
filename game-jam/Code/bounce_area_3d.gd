a52d9892436fd209f9e301656e734f066398cce1
KevinStranded <kevinsandstrom1999@gmail.com>
2025-11-08 14:43:54 -0800

    MoreSceneTesting

--- game-jam/Code/bounce_area_3d.gd
+++ game-jam/Code/bounce_area_3d.gd
@@ -12,8 +12,12 @@ func _process(_delta: float) -> void:
 func _physics_process(_delta: float) -> void:
 	pass
 
+
+
 func _on_body_entered(body: Node3D):
-	body.physics_material_override.bounce = 20
+	if body.is_in_group("Player"):
+		body.physics_material_override.bounce = 20
 
 func _on_body_exited(body: Node3D):
-	body.physics_material_override.bounce = 0
+	if body.is_in_group("Player"):
+		body.physics_material_override.bounce = 0
--- game-jam/Code/marble.gd
+++ game-jam/Code/marble.gd
@@ -33,7 +33,7 @@ func _physics_process(_delta: float) -> void:
 		# check if touching the floor (or really any CSGBox3D)
 		var bodies = get_colliding_bodies()
 		for body in bodies:
-			if body is CSGBox3D:
+			if body is CSGShape3D:
 				apply_impulse(Vector3(0, 1, 0) * jump_force)
 	current_move_force = move_force # reset current_move_force back to original move_force after boosting
 	
