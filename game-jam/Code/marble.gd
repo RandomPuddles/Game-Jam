f657e4fed4b3d8ec84304702874bb9e66d55babc
Ian Allenfort <ian.allenfort@wsu.edu>
2025-11-07 23:44:43 -0800

    Added boost to marble

--- game-jam/Code/marble.gd
+++ game-jam/Code/marble.gd
@@ -1,7 +1,9 @@
 extends RigidBody3D
 
-@export var move_force :float = 10.0
+@export var move_force: float = 10.0
+var current_move_force: float = 10.0
 @export var max_ang_speed :float = 50.0
+@export var boost_strength :float = 500.0
 var camera_3d : Camera3D
 
 func _ready():
@@ -12,14 +14,17 @@ func _process(_delta):
 	pass
 
 func _physics_process(_delta: float) -> void:
+	if Input.is_action_pressed("Boost"):
+		current_move_force *= 2
 	if Input.is_action_pressed("Forward"):
-		apply_torque(camera_3d.global_basis * Vector3(-1, 0, 0) * move_force)
+		apply_torque(camera_3d.global_basis * Vector3(-1, 0, 0) * current_move_force)
 	if Input.is_action_pressed("Backward"):
-		apply_torque(camera_3d.global_basis * Vector3(1, 0, 0) * move_force)
+		apply_torque(camera_3d.global_basis * Vector3(1, 0, 0) * current_move_force)
 	if Input.is_action_pressed("Right"):
-		apply_torque(camera_3d.global_basis * Vector3(0, 0, -1) * move_force)
+		apply_torque(camera_3d.global_basis * Vector3(0, 0, -1) * current_move_force)
 	if Input.is_action_pressed("Left"):
-		apply_torque(camera_3d.global_basis * Vector3(0, 0, 1) * move_force)
+		apply_torque(camera_3d.global_basis * Vector3(0, 0, 1) * current_move_force)
+	current_move_force = move_force
 
 	# clamp rotational velocity
 	if angular_velocity.length() > max_ang_speed:
