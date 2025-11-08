83eb92c2f936b8c92c3ff5448fdf19f81c1875bc
Ian Allenfort <ian.allenfort@wsu.edu>
2025-11-08 12:43:19 -0800

    Added time slow button

--- game-jam/Code/marble.gd
+++ game-jam/Code/marble.gd
@@ -5,7 +5,7 @@ var current_move_force: float = 10.0
 @export var max_ang_speed: float = 50.0
 @export var boost_multiple: float = 3.0
 var camera_3d : Camera3D
-@export var jump_force: float = 5.0
+@export var jump_force: float = 1.0
 @export var normal_friction: float = 3.15
 @export var ice_friction: float = 0.30
 @export var normal_bounce: float = 0.0
@@ -36,21 +36,23 @@ func _physics_process(_delta: float) -> void:
 			if body is CSGBox3D:
 				apply_impulse(Vector3(0, 1, 0) * jump_force)
 	current_move_force = move_force # reset current_move_force back to original move_force after boosting
-
+	
+	# progressively slow time while pressing the Slow button
+	if Input.is_action_pressed("Slow"):
+		if Engine.time_scale > 0:
+			Engine.time_scale -= 0.05
+			if Engine.time_scale < 0:
+				Engine.time_scale = 0
+	# resume time when the Resume button is pressed
+	if Input.is_action_pressed("Resume"):
+		if Engine.time_scale < 1:
+			Engine.time_scale += 0.05
+			if Engine.time_scale > 1:
+				Engine.time_scale = 1
+	
 	# clamp rotational velocity
 	if angular_velocity.length() > max_ang_speed:
 		angular_velocity = angular_velocity.normalized() * max_ang_speed
-	
-	# lower friction if on object in group "Ice"
-	var on_ice: bool = false
-	var bodies2 = get_colliding_bodies()
-	for body in bodies2:
-		if body.is_in_group("Ice"):
-			on_ice = true
-	if on_ice:
-		physics_material_override.friction = ice_friction
-	else:
-		physics_material_override.friction = normal_friction
 
 func _exit_tree() -> void:
 	pass
