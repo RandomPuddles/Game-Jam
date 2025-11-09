bc6cbc6aabbd54b1701a74745518ecb31b8576a7
kirbyDoesTaxes <hugmonster8@gmail.com>
2025-11-08 20:47:11 -0800

    we're tracking marbles 3d y level now
    long explanation:
    if marble goes under a bridge, bridge needs to be drawn on top of marble. if marble jaunts over bridge, bridge needs to be drawn under marble. we do not have the luxury to place events in the 3d spaces that tell the 2d space to reorganize their layering, so instead im checking the marble's 3d global y position. not the 2d y position the camera sees, that one is specific to the camera. this new one is specific to the 3d world the marble inhabits. it's okay if that doesnt make sense because im typing this hyped on hyperfocus energy THE IMPORTANT PART is im gonna need to keep track of what (3d) y levels mark the ball being below an object. this wont work for everything obvs but it's an easy way to help with layering. might need to add some for x and z but it depends on the type of object basically

--- game-jam/Scenes/bg_cube.gd
+++ game-jam/Scenes/bg_cube.gd
@@ -7,21 +7,19 @@ var ralseiBottom: float
 var cam3D: Camera3D
 var marb: Node3D
 var marbCoord: Vector2
+var marb3DYLevel: float
 var marbHeight: float
 var marbCoordBtoom: float
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	
-	#cam3D = get_tree().get_first_node_in_group("view").get_child(0).get_tree().get_first_node_in_group("the3DCamera")
-	#marb = get_tree().get_first_node_in_group("view").get_child(0).get_tree().get_first_node_in_group("Player")
-	#cam3D = marb.camera_3d
-	#cam3D = get_tree().current_scene.get_node("Camera3D")
 	cam3D = scene3D.get_child(0)
 	marb = scene3D.get_child(1).get_child(0)
 	marbCoord = cam3D.unproject_position(marb.position)
 	marbHeight = 25
 	marbCoordBtoom = marbCoord.y + marbHeight
+	marb3DYLevel = marb.global_position.y
 	
 	print(marbCoord)
 	#ralseiSprite = get_node("ralsei")
@@ -43,12 +41,20 @@ func _process(_delta: float) -> void:
 	#marb = scene3D.get_child(1).get_child(0)
 	marbCoord = cam3D.unproject_position(marb.position)
 	marbCoordBtoom = marbCoord.y + marbHeight
-	print(marbCoord)
+	#print(marbCoord)
+	marb3DYLevel = marb.global_position.y
+	print(marb3DYLevel)
 	
 	if marbCoordBtoom < ralseiBottom:
 		ralsei.z_index = 2
 	else:
 		ralsei.z_index = 0
+	pass
+	
+	if marb3DYLevel < 0:
+		bg.z_index = 2
+	else:
+		bg.z_index = 0
 	pass
 
 func check_ball_position():
