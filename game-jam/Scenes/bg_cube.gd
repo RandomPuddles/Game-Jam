1c60aed8547d8dd7607917e2d239e45550f9baf0
kirbyDoesTaxes <hugmonster8@gmail.com>
2025-11-08 18:56:17 -0800

    kinda got some code working for changing layers
    the height of the ball is currently managed manually, currently affect ralsei sprite only, and the rest of the script could be organized better. but its a start

--- game-jam/Scenes/bg_cube.gd
+++ game-jam/Scenes/bg_cube.gd
@@ -1,27 +1,35 @@
 extends Node2D
 
-@export var ralsei_ref: Node2D
-@export var bg_ref: Node2D
-var ralseiii: Node2D
-var marblePos: float
+@export var ralsei: Sprite2D
+var ralseiBottom: float
+@export var bg: Sprite2D
+@export var scene3D: Node3D
 var cam3D: Camera3D
 var marb: Node3D
 var marbCoord: Vector2
+var marbHeight: float
+var marbCoordBtoom: float
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	
-	cam3D = get_tree().get_first_node_in_group("view").get_child(0).get_tree().get_first_node_in_group("the3DCamera")
-	marb = get_tree().get_first_node_in_group("view").get_child(0).get_tree().get_first_node_in_group("Player")
+	#cam3D = get_tree().get_first_node_in_group("view").get_child(0).get_tree().get_first_node_in_group("the3DCamera")
+	#marb = get_tree().get_first_node_in_group("view").get_child(0).get_tree().get_first_node_in_group("Player")
 	#cam3D = marb.camera_3d
 	#cam3D = get_tree().current_scene.get_node("Camera3D")
+	cam3D = scene3D.get_child(0)
+	marb = scene3D.get_child(1).get_child(0)
 	marbCoord = cam3D.unproject_position(marb.position)
+	marbHeight = 25
+	marbCoordBtoom = marbCoord.y + marbHeight
 	
 	print(marbCoord)
 	#ralseiSprite = get_node("ralsei")
 	#bgsprite = get_node("bgDoodle")
 	
-	ralseiii = get_node("CanvasGroupTop/ralsei")
+	ralsei = get_node("CanvasGroupSprites/ralsei")
+	ralseiBottom = ralsei.position.y + (ralsei.texture.get_height() / 2)
+	bg = get_node("CanvasGroupSprites/theBackground")
 	
 	pass # Replace with function body.
 
@@ -29,8 +37,18 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	#marblePos = 0
+	#cam3D = get_tree().get_first_node_in_group("view").get_child(0).get_tree().get_first_node_in_group("the3DCamera")
+	#marb = get_tree().get_first_node_in_group("view").get_child(0).get_tree().get_first_node_in_group("Player")
+	#cam3D = scene3D.get_child(0)
+	#marb = scene3D.get_child(1).get_child(0)
 	marbCoord = cam3D.unproject_position(marb.position)
+	marbCoordBtoom = marbCoord.y + marbHeight
 	print(marbCoord)
+	
+	if marbCoordBtoom < ralseiBottom:
+		ralsei.z_index = 2
+	else:
+		ralsei.z_index = 0
 	pass
 
 func check_ball_position():
