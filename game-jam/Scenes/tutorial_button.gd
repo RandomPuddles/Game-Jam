13355cb59b3990e9938348f10687df8ec5f7072f
kirbyDoesTaxes <hugmonster8@gmail.com>
2025-11-09 18:14:04 -0800

    tutorial scene (its really good)

--- game-jam/Scenes/tutorial_button.gd
+++ game-jam/Scenes/tutorial_button.gd
@@ -1,5 +1,6 @@
 extends TextureButton
 
+var scene: PackedScene = preload("res://Scenes/tutorial.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,4 +15,5 @@ func _on_pressed() -> void:
 	# bring up page with instructions
 	# and maybe story (ALL AT ONCE)
 	# like old game manual
+	get_tree().change_scene_to_packed(scene)
 	pass
