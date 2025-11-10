5c0a1133a526c881b10efe4d6a7a6c9aea815168
Ian Allenfort <ian.allenfort@wsu.edu>
2025-11-09 17:27:08 -0800

    Levels load into eachother

---
+++ game-jam/Scenes/level_six_loader.gd
@@ -0,0 +1,16 @@
+extends Area3D
+
+var level: PackedScene = preload("res://Level2Perchance.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+func _on_body_entered(body: Node3D) -> void:
+	if body.is_in_group("Player"):
+		get_tree().change_scene_to_packed(level)
